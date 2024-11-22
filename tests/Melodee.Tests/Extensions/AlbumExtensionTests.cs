f6fc6c731713cb288a29b32deb7002194a9bea95
Steven Hildreth <steven@hixi.org>
2024-11-22 16:34:18 -0600

    milestone commit

--- tests/Melodee.Tests/Extensions/AlbumExtensionTests.cs
+++ tests/Melodee.Tests/Extensions/AlbumExtensionTests.cs
@@ -57,7 +57,7 @@ public class AlbumExtensionTests
                     CrcHash = Crc32.Calculate(new FileInfo(@"/melodee_test/inbound/00-k 2024/03-holy_truth-flako_el_dark_cowboy.mp3")),
                     File = new FileSystemFileInfo
                     {
-                        Name = "03-holy_truth-flako_el_dark_cowboy.mp3\"",
+                        Name = "03-holy_truth-flako_el_dark_cowboy.mp3",
                         Size = 12343
                     },
                     Tags = new[]
