64b9b10e933ceea021938ea6eb9ee823a33fec26
Steven Hildreth <steven@hixi.org>
2025-01-01 14:28:19 -0600

    milestone commit

---
+++ src/Melodee.Common/Configuration/Extensions/IMelodeeConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+using Melodee.Common.Constants;
+
+namespace Melodee.Common.Configuration.Extensions;
+
+public static class IMelodeeConfigurationExtensions
+{
+    public static short DefaultPageSize(this IMelodeeConfiguration configuration) 
+        => configuration.GetValue<short>(SettingRegistry.DefaultsPageSize);
+    
+    public static int[] DefaultPageSizeOptions(this IMelodeeConfiguration configuration)
+    {
+        var pageSize = configuration.DefaultPageSize();
+        return [pageSize, pageSize * 2, pageSize * 5, pageSize * 10];
+    }   
+}
